ec7631c673c359460b7eb667dcfa8c6edbbc8443
htaox <htaox@hotmail.com>
2014-11-04 16:59:40 -0500

    Making progress

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -8,42 +8,62 @@ using namespace v8;
 
 Persistent<Function> Loader::constructor;
 
-Loader::Loader(double value) : value_(value) {
+Loader::Loader(std::string value) : value_(value){
 }
 
 Loader::~Loader() {
 }
 
 void Loader::Init(Handle<Object> exports) {
+  
   // Prepare constructor template
   Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
   tpl->SetClassName(String::NewSymbol("Loader"));
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
+  
   // Prototype
   tpl->PrototypeTemplate()->Set(String::NewSymbol("bulkLoad"),
       FunctionTemplate::New(Bulkload)->GetFunction());
+
   constructor = Persistent<Function>::New(tpl->GetFunction());
   exports->Set(String::NewSymbol("Loader"), constructor);
 }
 
+/*
+v8::String vs std::string
+=========================
+
+v8::String::Utf8Value param1(args[0]->ToString());
+std::string from = std::string(*param1);
+and if your just trying to convert a std::string to a v8::String then do
+
+std::string something("hello world"); 
+Handle<Value> something_else = String::New( something.c_str() );
+
+Reference:
+http://code.tutsplus.com/tutorials/writing-nodejs-addons--cms-21771
+*/
 Handle<Value> Loader::New(const Arguments& args) {
   HandleScope scope;
 
   if (args.IsConstructCall()) {
     // Invoked as constructor: `new Loader(...)`
-    double value = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
-    Loader* obj = new Loader(value);
+    v8::String::Utf8Value str(args[0]->ToString());
+	std::string s(*str);
+	
+	Loader* obj = new Loader(s);
     obj->Wrap(args.This());
     return args.This();
   } else {
     // Invoked as plain function `Loader(...)`, turn into construct call.
     const int argc = 1;
     Local<Value> argv[argc] = { args[0] };
-    return scope.Close(constructor->NewInstance(argc, argv));
+	return scope.Close(constructor->NewInstance(argc, argv));
   }
 }
 
-int Loader::OpenFile(std::string path, std::stringstream ss) {
+int Loader::OpenFile(std::string path) {
+  std::stringstream ss;
   std::ifstream ifs;
   char* buffer;
   std::string temp;
@@ -51,7 +71,10 @@ int Loader::OpenFile(std::string path, std::stringstream ss) {
   
   try{
 		ifs.open (path.c_str(), std::ios::binary );
-		if (!ifs) return 1;
+		if (!ifs) {
+			retval_.assign(path + " does not exist");
+			return 1;
+		}
 		// get length of file:
 		ifs.seekg (0, std::ios::end);
 		length = ifs.tellg();
@@ -59,9 +82,7 @@ int Loader::OpenFile(std::string path, std::stringstream ss) {
 		// allocate memory:
 		buffer = new (std::nothrow) char [length]; 
 		if (buffer == 0){
-		  //get_time(st, timestamp);
-		  //out_l << timestamp << "|Error thrown by bad allocation attempt.  Buffer requested: " << length << "\n"; 
-		  //out_l.flush();
+		  retval_.assign(path + " is empty");
 		  return 1;
 		}
 		ifs.read(buffer,length);  
@@ -70,21 +91,20 @@ int Loader::OpenFile(std::string path, std::stringstream ss) {
 		delete[] buffer;
   }
   catch (std::exception& e){
-		//get_time(st, timestamp);
-		//out_l << timestamp << "|Error-> " << e.what() << "\n";
-		//out_l.flush();
+		retval_.assign(path+" error caught-> "+e.what());
+		return 1;
   }
-  return length;
+  retval_.assign(path+" size-> "+std::to_string(length));
+  return 0;
 }
 
 Handle<Value> Loader::Bulkload(const Arguments& args) {
   HandleScope scope;
 
   Loader* obj = ObjectWrap::Unwrap<Loader>(args.This());
-  //obj->value_ += 1;
-  std::string s;
-  std::stringstream ss;
+  
+  obj->OpenFile(obj->value_);
 
-  return scope.Close(Number::New(obj->value_));
+  return scope.Close(v8::String::New(obj->retval_.c_str()));
 }
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,15 @@
 
 using namespace v8;
 
+/**
+	@usage
+	var NgraphLmdb = require('./x64/Debug/NgraphLmdb');
+	var loader = new NgraphLmdb.Loader('some/path/to/file');
+	//Execute!
+	loader.bulkLoad();
+**/
 void InitAll(Handle<Object> exports) {
   Loader::Init(exports);
 }
 
-NODE_MODULE(NodeAddon, InitAll)
+NODE_MODULE(NgraphLmdb, InitAll)
